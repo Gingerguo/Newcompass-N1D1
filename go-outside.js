d7955ed16d3967aa3dde44c70c982c03a5be57ae
gingerguo <guomimai@gmail.com>
2016-10-05 16:24:16 +0000

    commit twice

--- go-outside.js
+++ go-outside.js
@@ -1,14 +1,16 @@
 var raining = true;
-var cold = false;
+var temperature = 12;
 
 if(raining){
   console.log("Do not forget your umbrella");
 }
 
-if(cold){
+if(temperature < 15){
   console.log('Make sure you pick a scarf!');
+} else if (temperature < 15){
+  console.log("Short sleeves won't cut it!");
 } else {
   console.log('Short sleeves are fine.');
 }
 
-console.log('Now you are ready to go outside');
+console.log('Now you are ready to go outside!');
