e985060bcae040b1f32181e35a5b9967f87af4f1
gingerguo <guomimai@gmail.com>
2016-10-05 17:30:31 +0000

    kitchen stuffs

--- packing-list.js
+++ packing-list.js
@@ -3,3 +3,7 @@ console.log("Kitchen stuff to pack");
 for (var i = 0; i < packingList.length; i++){
   console.log(packingList[i]);
 }
+
+for (var i = packingList.length - 1; i >= 0; i--){
+  console.log(packingList[i]);
+}
