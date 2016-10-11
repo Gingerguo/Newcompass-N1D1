a4cad69efad3092aee9f6381a4d5cbc3f6cf854f
gingerguo <guomimai@gmail.com>
2016-10-11 18:43:58 +0000

    change

---
+++ day2/print-in-frame.js
@@ -0,0 +1,35 @@
+function printInFrame(list) {
+  var list = list.split(' ');
+  console.log(list);
+  var longest = longestStr(list).length;
+
+  var border = repeat('*', longest);
+
+  console.log(border);
+  for (word of list) {
+    console.log('* ' + word + repeat(' ', longest - word.length + 1) + '*');
+  }
+  console.log(border);
+}
+
+function repeat(str, times) {
+  var result = str;
+
+  for (var i = 0; i < times; i++) {
+    result += str;
+  }
+
+  return result;
+}
+
+function longestStr(list) {
+
+  var longest = Math.max(...list);
+  return longest;
+}
+
+// Test driver code, do not modify
+printInFrame('May the force be with you');
+printInFrame('Here\'s Johnny!');
+printInFrame('Supercalifragalisticexpialadocious');
+printInFrame('Lost, like tears in the rain');
